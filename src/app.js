8df866bbfdd3e0e94eb592789d85ca9edca55621
Yayati7 <singhpsby@gmail.com>
2026-02-08 00:40:32 +0530

    Fix cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 /* ---------- MIDDLEWARE ---------- */
 app.use(cors({
-  origin: "https://mini-social-frontend-three.vercel.app/",
+  origin: "https://mini-social-frontend-three.vercel.app",
   credentials: true
 }));
 
